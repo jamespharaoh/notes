34946b94bc64949a097661de21b9d3c62c30f980
James Pharaoh <james@phsys.co.uk>
2012-04-07 14:00:49 +0200

    improve eunit coverage

---
+++ test/misc/notes_random_test.erl
@@ -0,0 +1,34 @@
+-module (notes_random_test).
+
+-include ("notes_test.hrl").
+
+% macros
+
+-define (MOCK_MODULES, [
+	crypto ]).
+
+-define (TARGET,
+	notes_random).
+
+random_id_test () ->
+
+	RandomId =
+		"abcdefghijklmnop",
+
+	?EXPECT,
+
+		lists:foreach (
+			fun (Char) ->
+				em:strict (Em, crypto, rand_uniform,
+					[ 0, 26 ],
+					{ return, Char - $a })
+				end,
+			RandomId),
+
+	?REPLAY,
+
+		?assertEqual (
+			RandomId,
+			?TARGET:random_id ()),
+
+	?VERIFY.
